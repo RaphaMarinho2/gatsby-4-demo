2cb02571d0b18c6427bb54e16e83ae77f817c9fd
Raphael Marinho <raphael.marinho@acct.global>
2022-11-03 14:51:01 -0300

    added plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,8 @@ module.exports = {
     title: `Gatsby 9689ff demo`,
   },
   plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
     {
       resolve: `gatsby-source-filesystem`,
       options: {
